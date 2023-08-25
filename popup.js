81725000d95c5ed7ac7af8cb8f4d812cc82513bf
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-08-24 21:44:10 -0700

    Nailed the functionality

--- popup.js
+++ popup.js
@@ -1,16 +1,63 @@
+// Get references to elements
 const startButton = document.getElementById("startTimer");
 const startBreakButton = document.getElementById("startBreak");
+const endBreakButton = document.getElementById("endBreak");
 
+const timerContent = document.getElementById("countdown");
+const inputContent = document.getElementById("inputSection");
+
+let timerInterval; // Declare the timerInterval variable
+
+// After the break timer is over
+// Show input content and populate input fields with previous input values
+function showInputContent(workTime, breakTime) {
+    timerContent.style.display = "none";
+    inputContent.style.display = "block";
+
+    const workTimeInput = document.getElementById("workTime");
+    const breakTimeInput = document.getElementById("breakTime");
+
+    if (workTimeInput && breakTimeInput) {
+        workTimeInput.value = workTime;
+        breakTimeInput.value = breakTime;
+    }
+}
+
+// Timer logic
+function startTimer(duration, callback) {
+    let totalTime = duration * 60;  // Convert minutes to seconds
+    const displayTimer = document.getElementById("timer");
+
+    const updateTimerDisplay = () => {
+        if (totalTime >= 0) {
+            const minutes = Math.floor(totalTime / 60);
+            const seconds = totalTime % 60;
+            displayTimer.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+            totalTime--;
+
+            if (totalTime < 0) {
+                clearInterval(timerInterval);
+                if (callback) callback(); // Call the passed callback function when timer runs out
+            }
+        }
+    };
+
+    updateTimerDisplay();
+    timerInterval = setInterval(updateTimerDisplay, 1000); // Initialize timerInterval here
+
+    // At the end of the timer, if the callback is given:
+    if (totalTime < 0) {
+        clearInterval(timerInterval);
+        if (callback) callback();
+    }
+}
+
+
+// Event listener for start button
 startButton.addEventListener("click", function() {
     const workTimeInput = document.getElementById("workTime");
     const inputSection = document.getElementById("inputSection");
     const countdownSection = document.getElementById("countdown");
-    const displayTimer = document.getElementById("timer");
-
-    if (!workTimeInput || !inputSection || !countdownSection || !displayTimer) {
-        console.error("One or more elements are missing.");
-        return;
-    }
 
     const workTime = parseInt(workTimeInput.value) || 0;
 
@@ -19,40 +66,20 @@ startButton.addEventListener("click", function() {
 
     startTimer(workTime, function() {
         startBreakButton.style.display = "block";
+        startBreakButton.addEventListener("click", function() {
+            const breakTimeInput = document.getElementById("breakTime");
+            const breakTime = parseInt(breakTimeInput.value) || 0;
+
+            startBreakButton.style.display = "none";
+            startTimer(breakTime, function() {
+                startBreakButton.style.display = "none";
+                endBreakButton.style.display = "block";
+                endBreakButton.addEventListener("click", function() {
+                    inputSection.style.display = "block";
+                    endBreakButton.style.display = "none";
+                    countdownSection.style.display = "none";
+                });
+            });
+        });
     });
 });
-
-startBreakButton.addEventListener("click", function() {
-    const breakTimeInput = document.getElementById("breakTime");
-    if (!breakTimeInput) {
-        console.error("Break time input not found.");
-        return;
-    }
-
-    const breakTime = parseInt(breakTimeInput.value) || 0;
-
-    startBreakButton.style.display = "none"; // Hide the start break button
-
-    startTimer(breakTime, function() {
-        alert("Break is over!");
-    });
-});
-
-function startTimer(duration, callback) {
-    let totalTime = duration * 60;  // Convert minutes to seconds
-    const displayTimer = document.getElementById("timer");
-
-    const updateTimerDisplay = () => {
-        const minutes = Math.floor(totalTime / 60);
-        const seconds = totalTime % 60;
-        displayTimer.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
-        totalTime--;
-
-        if (totalTime < 0) {
-            clearInterval(timerInterval);
-            if (callback) callback(); // Call the passed callback function when timer runs out
-        }
-    };
-
-    const timerInterval = setInterval(updateTimerDisplay, 1000);
-}
