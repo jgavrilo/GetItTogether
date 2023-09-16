167092107061b18bb80f8101d3edb5321d4d638e
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-09-16 15:30:50 -0700

    Settings with tabs

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     // Load saved colors from local storage
     const primaryColor = localStorage.getItem('primaryColor') || '#1a1a1a';
     const secondaryColor = localStorage.getItem('secondaryColor') || '#ffffff';
-
+    const buttonColor = localStorage.getItem('buttonColor') || '#3498db';
+    const buttonTextColor =  localStorage.getItem('buttonTextColor') || '#000000';
     // Set the input fields with the saved colors
     document.getElementById('primaryColor').value = primaryColor;
     document.getElementById('secondaryColor').value = secondaryColor;
+    document.getElementById('buttonColor').value = buttonColor;
+    document.getElementById('buttonTextColor').value = buttonTextColor;
+
+    // Attach event listeners to the tab buttons
+    document.getElementById('colorsTab').addEventListener('click', function(event) {
+        openTab(event, 'Colors');
+    });
+
+    document.getElementById('syncTab').addEventListener('click', function(event) {
+        openTab(event, 'Sync');
+    });
 
     // Listen for form submission
     document.getElementById('settingsForm').addEventListener('submit', function(event) {
@@ -14,12 +26,30 @@ document.addEventListener('DOMContentLoaded', function() {
         // Get selected colors
         const selectedPrimaryColor = document.getElementById('primaryColor').value;
         const selectedSecondaryColor = document.getElementById('secondaryColor').value;
+        const selectedButtonColor = document.getElementById('buttonColor').value;
+        const selectedButtonTextColor = document.getElementById('buttonTextColor').value;
 
         // Save selected colors to local storage
         localStorage.setItem('primaryColor', selectedPrimaryColor);
         localStorage.setItem('secondaryColor', selectedSecondaryColor);
+        localStorage.setItem('buttonColor', selectedButtonColor);
+        localStorage.setItem('buttonTextColor', selectedButtonTextColor);
 
         // Update colors immediately
         updateColors();
     });
 });
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
--- src/js/theme.js
+++ src/js/theme.js
@@ -2,6 +2,8 @@
 function updateColors() {
     document.documentElement.style.setProperty('--primary-color', localStorage.getItem('primaryColor') || '#1a1a1a');
     document.documentElement.style.setProperty('--secondary-color', localStorage.getItem('secondaryColor') || '#ffffff');
+    document.documentElement.style.setProperty('--button-color', localStorage.getItem('buttonColor') || '#3498db');
+    document.documentElement.style.setProperty('--button-text-color', localStorage.getItem('buttonTextColor') || '#000000');
 }
 
 // Initial update
@@ -9,7 +11,7 @@ updateColors();
 
 // Listen for local storage changes
 window.addEventListener('storage', function(event) {
-    if (event.key === 'primaryColor' || event.key === 'secondaryColor') {
+    if (['primaryColor', 'secondaryColor', 'buttonColor', 'buttonTextColor'].includes(event.key)) {
         updateColors();
     }
 });
