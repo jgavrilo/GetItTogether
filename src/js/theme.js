c390ec4326178567b6911c8b26c26ba7238815fe
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-09-17 16:58:59 -0700

    Formatting

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,9 +1,9 @@
 // Function to update colors
 function updateColors() {
-    document.documentElement.style.setProperty('--primary-color', localStorage.getItem('primaryColor') || '#1a1a1a');
-    document.documentElement.style.setProperty('--secondary-color', localStorage.getItem('secondaryColor') || '#ffffff');
-    document.documentElement.style.setProperty('--button-color', localStorage.getItem('buttonColor') || '#3498db');
-    document.documentElement.style.setProperty('--button-text-color', localStorage.getItem('buttonTextColor') || '#000000');
+    document.documentElement.style.setProperty('--primary-color', localStorage.getItem('primaryColor'));
+    document.documentElement.style.setProperty('--secondary-color', localStorage.getItem('secondaryColor'));
+    document.documentElement.style.setProperty('--third-color', localStorage.getItem('thirdColor'));
+    document.documentElement.style.setProperty('--fourth-color', localStorage.getItem('fourthColor'));
 }
 
 // Initial update
@@ -11,7 +11,7 @@ updateColors();
 
 // Listen for local storage changes
 window.addEventListener('storage', function(event) {
-    if (['primaryColor', 'secondaryColor', 'buttonColor', 'buttonTextColor'].includes(event.key)) {
+    if (['primaryColor', 'secondaryColor', 'thirdColor', 'fourthColor'].includes(event.key)) {
         updateColors();
     }
 });
