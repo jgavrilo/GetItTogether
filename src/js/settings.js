95a94ca7e1b4f8f438a2fd57d07a6acb5b05591e
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-09-19 16:47:08 -0700

    Google account login button/tab

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,34 +1,82 @@
+// Existing function to open tabs
 function openTab(evt, tabName) {
-    const tabcontent = document.getElementsByClassName("tabcontent");
-    Array.from(tabcontent).forEach(tab => {
-      tab.style.display = "none";
-    });
-  
-    const tablinks = document.getElementsByClassName("tablinks");
-    Array.from(tablinks).forEach(link => {
-      link.className = link.className.replace(" active", "");
-    });
-  
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  
-    // Emit a custom event to notify that the tab has been switched
-    const tabSwitchEvent = new Event('tabSwitched');
-    document.dispatchEvent(tabSwitchEvent);
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    // Set initial active tab
-    const initialTabName = 'Colors';  // Change this to the name of the tab you want to be active initially
-    document.getElementById(initialTabName).style.display = 'block';
-  
-    // Attach event listeners to the tab buttons
-    document.getElementById('colorsTab').addEventListener('click', function(event) {
-      openTab(event, 'Colors');
-    });
-  
-    document.getElementById('alarmsTab').addEventListener('click', function(event) {
-      openTab(event, 'Alarms');
-    });
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  Array.from(tabcontent).forEach(tab => {
+    tab.style.display = "none";
   });
-  
+
+  const tablinks = document.getElementsByClassName("tablinks");
+  Array.from(tablinks).forEach(link => {
+    link.className = link.className.replace(" active", "");
+  });
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  // Emit a custom event to notify that the tab has been switched
+  const tabSwitchEvent = new Event('tabSwitched');
+  document.dispatchEvent(tabSwitchEvent);
+}
+
+// Existing DOMContentLoaded event listener
+document.addEventListener('DOMContentLoaded', function() {
+  // Set initial active tab
+  const initialTabName = 'Colors';
+  document.getElementById(initialTabName).style.display = 'block';
+
+  // Attach event listeners to the tab buttons
+  document.getElementById('colorsTab').addEventListener('click', function(event) {
+    openTab(event, 'Colors');
+  });
+
+  document.getElementById('alarmsTab').addEventListener('click', function(event) {
+    openTab(event, 'Alarms');
+  });
+
+  document.getElementById('googleAccountTab').addEventListener('click', function(event) {
+    openTab(event, 'GoogleAccount');
+  });
+
+  // New: Update Google Account button
+  updateGoogleAccountButton();
+});
+
+// New: Variable to keep track of login status
+let isLoggedIn = false;
+
+// New: Function to check if user is logged in
+function checkLoginStatus() {
+  // Your logic to check login status
+  return isLoggedIn;
+}
+
+// New: Function to update button based on login status
+function updateGoogleAccountButton() {
+  const button = document.getElementById('googleAccountButton');
+
+  if (!button) {
+    console.error("googleAccountButton is null");
+    return;
+  }
+
+  isLoggedIn = checkLoginStatus();
+
+  if (isLoggedIn) {
+    button.textContent = 'Disconnect from Google Account';
+    button.onclick = function() {
+      isLoggedIn = false;
+      updateGoogleAccountButton();
+    };
+  } else {
+    button.textContent = 'Connect to Google Account';
+    button.onclick = function() {
+      isLoggedIn = true;
+      updateGoogleAccountButton();
+    };
+  }
+}
+
+// New: Listen for the custom tabSwitched event to update the button
+document.addEventListener('tabSwitched', function() {
+  updateGoogleAccountButton();
+});
